9c7cf07230a72f89343803470c543df35cfc581b
Maximilian Graf <maxgraf27@gmail.com>
2023-12-26 08:24:15 +0100

    init app and class for HR Client

--- bluetooth_heartrate_api/bluetooth_hr.py
+++ bluetooth_heartrate_api/bluetooth_hr.py
@@ -1,68 +1,50 @@
 import time
 import csv
-from bleak import BleakClient
 import asyncio
-from bleak import BleakScanner
+from fastAPI import FastAPI, WebSocket
+from bleak import BleakClient, BleakScanner
 
-# discover devices
-async def discover():
-    devices = await BleakScanner.discover()
-
-    num_devices = 0
-    for d in devices:
-        if "fenix" in d.__str__():
-            print(d)
-        else:
-            num_devices += 1
-    
-    print(f"Found {num_devices} devices")
-
-asyncio.run(discover())
-
-
-address = "B4:C2:6A:A9:A4:04"
 HRM_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
-
-# connect to device
-async def main(address):
-    async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(HRM_UUID)
-        print("Model Number: {0}".format("".join(map(chr, model_number))))
-
-asyncio.run(main(address))
-
-# connect and read data
-
-async def connect_and_read(address):
-    async with BleakClient(address) as client:
-        if await client.is_connected():
-            print("Connected to device")
-            await client.start_notify(HRM_UUID, callback_handler)
-
 
 async def callback_handler(sender, data):
     # Process the heart rate data received
-    print("Heart Rate:", int.from_bytes(data, byteorder='little'))
+    print("Heart Rate: ", int.from_bytes(data, byteorder='little'))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(connect_and_read(address))
+class HeartRateClient():
+    def __init__(self):
+        pass
 
-# write data to file
-heart_rates = []
+    async def show_devices(self):
+        devices = await BleakScanner.discover()
+        num_devices = 0
+        for d in devices:
+            if "fenix" in d.__str__():
+                print(d)
+            else:
+                num_devices += 1
+        print(f"Found {num_devices} devices")  
 
-# def callback_handler(sender, data):
-#     heart_rate = int.from_bytes(data, byteorder='little')
-#     heart_rates.append(heart_rate)
-#     print("Heart Rate:", heart_rate)
+    def set_adress(self, address):
+        self.address = address
+    
+    async def connect_to_adress(self):
+        async with BleakClient(self.address) as client:
+            model_number = await client.read_gatt_char(HRM_UUID)
+            print("Model Number: {0}".format("".join(map(chr, model_number))))
 
-# Run for 1 minute
-start_time = time.time()
-while time.time() - start_time < 60:
-    loop.run_until_complete(connect_and_read(address))
+    async def connect_and_read(self):
+        async with BleakClient(self.address) as client:
+            if client.is_connected:
+                print("Connected to device")
+                await client.start_notify(HRM_UUID, callback_handler)
 
-# Save to CSV
-with open('heart_rate_data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Heart Rate"])
-    for rate in heart_rates:
-        writer.writerow([rate])
+if __name__ == "__main__":
+    hr_client = HeartRateClient()
+    asyncio.run(hr_client.show_devices())
+    hr_client.set_adress("B4:C2:6A:A9:A4:04")
+    # asyncio.run(hr_client.connect_to_adress())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(hr_client.connect_and_read())
+
+    
